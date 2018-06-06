03c92f82cf96dff4f53b108f612784b96bb1aeff
tienloc <tienlocbui1110@gmail.com>
2018-06-06 17:19:54 +0700

    init

---
+++ app/src/main/java/vn/edu/hcmus/ldolphin/MainActivity.java
@@ -0,0 +1,25 @@
+package vn.edu.hcmus.ldolphin;
+
+import android.annotation.SuppressLint;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_view);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+
+    }
+
+}
