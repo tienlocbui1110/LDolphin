931f2ca44aeee5e4440bf5940b7aad345e0edc28
DatDuc <datducnguyenhuu@gmail.com>
2018-06-25 21:49:14 +0700

    update user routes

--- server/lDolphin/controllers/userController.js
+++ server/lDolphin/controllers/userController.js
@@ -1,3 +1,23 @@
 var controller = {};
 
 var models = require('../models');
+
+controller.getAll = function(callback) {
+    models.User
+        .findAll()
+        .then(function(users) {
+            callback(users);
+        });
+}
+
+controller.getById = function(userId, callback) {
+    models.User
+        .findOne({
+            where: {
+                ID: userId
+            },
+        })
+        .then(function(user) {
+            callback(user);
+        });
+}
